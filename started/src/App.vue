5aeb6dfcc21e68dbac5cee790c00fa7e103e9d58
CasJuan <castrojuanig25@gmail.com>
2025-01-02 22:03:37 -0300

    [add] pasaje de props

--- started/src/App.vue
+++ started/src/App.vue
@@ -3,9 +3,12 @@
   <CarComposition />
   <hr/>
   <CarOptions/>
+  <hr/>
+  <Car :power="power"/>
 </template>
 
 <script>
+import Car from "./components/Car.vue";
 import CarComposition from "./components/CarComposition.vue";
 import CarOptions from "./components/CarOptions.vue";
 
@@ -13,7 +16,15 @@ export default {
   components: {
     CarComposition,
     CarOptions,
+    Car
   },
+  setup(){
+    let power = 38;
+
+    return{
+      power,
+    }
+  }
 };
 </script>
 
---
+++ started/src/components/Car.vue
@@ -0,0 +1,38 @@
+<template>
+    <h2>Coche - Composition API</h2>
+    <p>Marca: {{ brand }}</p>
+    <p>Modelo: {{ model }}</p>
+
+    <p> Potencias : {{ power }} </p>
+
+</template>
+
+<script>
+
+export default {
+
+    props:{
+        power:{
+            type:Number,
+            default: 60,
+        }
+    },
+    
+    setup (props) {
+        const brand = "Audi";
+        const model = "A4";
+
+        console.log(props);
+
+        return {
+            brand,
+            model,
+        };
+    },
+
+};
+</script>
+
+<style>
+
+</style>
